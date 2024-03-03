9b12f4946ce21c7166e0c69119ac311769b30df0
brettabamonte <abamonteb@gmail.com>
2024-03-03 11:12:28 -0500

    Add get, set, del command functionality. Implement map data structure to safe data for now.

--- server.cpp
+++ server.cpp
@@ -12,6 +12,8 @@
 #include <vector>
 #include <stdbool.h>
 #include <poll.h>
+#include <map>
+#include <string>
 
 // FUTURE UPDATES:
 // 1. Use epoll instead of poll
@@ -25,6 +27,15 @@ enum
     STATE_RES = 1, // Sending requests
     STATE_END = 2  // Terminate connection
 };
+
+enum
+{
+    RES_OK = 0,
+    RES_ERR = 1,
+    RES_NX = 2,
+};
+
+static std::map<std::string, std::string> g_map;
 
 struct Connection
 {
@@ -127,6 +138,117 @@ static int32_t accept_new_conn(std::vector<Connection *> &fd_to_connection, int 
     return 0;
 }
 
+static uint32_t do_get(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    if (!g_map.count(cmd[1]))
+    {
+        return RES_NX;
+    }
+
+    std::string &val = g_map[cmd[1]];
+    assert(val.size() <= k_max_msg);
+    memcpy(res, val.data(), val.size());
+    *reslen = (uint32_t)val.size();
+    return RES_OK;
+}
+
+static uint32_t do_set(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    (void)res;
+    (void)reslen;
+    g_map[cmd[1]] = cmd[2];
+    return RES_OK;
+}
+
+static uint32_t do_del(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    (void)res;
+    (void)reslen;
+    g_map.erase(cmd[1]);
+    return RES_OK;
+}
+
+static int32_t parse_req(const uint8_t *data, size_t len, std::vector<std::string> &out)
+{
+    if (len < 4)
+    {
+        return -1;
+    }
+
+    uint32_t n = 0;
+    memcpy(&n, &data[0], 4);
+    if (n > k_max_msg)
+    {
+        return -1;
+    }
+
+    size_t pos = 4;
+    while (n--)
+    {
+        if (pos + 4 > len)
+        {
+            return -1;
+        }
+        uint32_t sz = 0;
+        memcpy(&sz, &data[pos], 4);
+        if (pos + 4 + sz > len)
+        {
+            return -1;
+        }
+
+        out.push_back(std::string((char *)&data[pos + 4], sz));
+        pos += 4 + sz;
+    }
+
+    if (pos != len)
+    {
+        return -1; // There is misc trailing garbage
+    }
+    return 0;
+}
+
+static bool cmd_is(const std::string &word, const char *cmd)
+{
+    return 0 == strcasecmp(word.c_str(), cmd);
+}
+
+static int32_t do_request(
+    const uint8_t *req, uint32_t reqlen,
+    uint32_t *rescode, uint8_t *res, uint32_t *reslen)
+{
+    std::vector<std::string> cmd;
+
+    if (0 != parse_req(req, reqlen, cmd))
+    {
+        msg("bad req");
+        return -1;
+    }
+
+    if (cmd.size() == 2 && cmd_is(cmd[0], "get"))
+    {
+        *rescode = do_get(cmd, res, reslen);
+    }
+    else if (cmd.size() == 3 && cmd_is(cmd[0], "set"))
+    {
+        *rescode = do_set(cmd, res, reslen);
+    }
+    else if (cmd.size() == 2 && cmd_is(cmd[0], "del"))
+    {
+        *rescode = do_del(cmd, res, reslen);
+    }
+    else
+    {
+        // Cmd isn't recognized
+        *rescode = RES_ERR;
+        const char *msg = "Unknown cmd";
+        strcpy((char *)res, msg);
+        *reslen = strlen(msg);
+        return 0;
+    }
+
+    return 0;
+}
+
 static bool try_one_request(Connection *conn)
 {
     // Try to parse a request from buffer
@@ -153,13 +275,23 @@ static bool try_one_request(Connection *conn)
         return false;
     }
 
-    // Get request, do something with it
-    printf("client says: %.%s\n", len, &conn->read_buffer[4]);
+    // 1 request, generate response
+    uint32_t rescode = 0;
+    uint32_t wlen = 0;
+    int32_t err = do_request(
+        &conn->read_buffer[4], len,
+        &rescode, &conn->write_buffer[4 + 4], &wlen);
 
-    // Generate echoing response
-    memcpy(&conn->write_buffer[0], &len, 4);
-    memcpy(&conn->write_buffer[4], &conn->read_buffer[4], len);
-    conn->write_buffer_size = 4 + len;
+    if (err)
+    {
+        conn->state = STATE_END;
+        return false;
+    }
+
+    wlen += 4;
+    memcpy(&conn->write_buffer[0], &wlen, 4);
+    memcpy(&conn->write_buffer[4], &rescode, 4);
+    conn->write_buffer_size = 4 + wlen;
 
     // Remove the request from the buffer
     // FIXME: memmove in prod isn't efficient
