322e77bb2972e3b8338270342e9df92f1f77fdab
brettabamonte <abamonteb@gmail.com>
2024-03-04 21:29:26 -0500

    Add lookup function. Searchs through hashtable's array of linked lists

--- hashtable.cpp
+++ hashtable.cpp
@@ -10,3 +10,39 @@ static void h_init(HTab *htab, size_t n)
     htab->mask = n - 1;
     htab->size = 0;
 }
+
+static void h_insert(HTab *htab, HNode *node)
+{
+    size_t pos = node->hcode & htab->mask; // slot index
+    HNode *next = htab->tab[pos];          // prepend the list
+    node->next = next;
+    htab->tab[pos] = node;
+    htab->size++;
+}
+
+// Lookup HNode in HTab
+// Returns: Pointer to HNode
+static HNode **h_lookup(HTab *htab, HNode *key, bool (*eq)(HNode *, HNode *))
+{
+    // Check if a table is init for hashtable
+    if (!htab->tab)
+    {
+        return NULL;
+    }
+
+    // Get position of key
+    size_t pos = key->hcode & htab->mask;
+
+    HNode **from = &htab->tab[pos]; // incoming pointer to the result
+
+    // Loop through chained array til we find matching node
+    for (HNode *cur; (cur = *from) != NULL; from = &cur->next)
+    {
+        if (cur->hcode == key->hcode && eq(cur, key))
+        {
+            return from;
+        }
+    }
+
+    return NULL;
+}
