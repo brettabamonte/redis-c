2d1085f57277da9360650437cf0632fbcd1c184f
brettabamonte <abamonteb@gmail.com>
2024-03-04 21:35:22 -0500

    Add Hash Map data structure and signature for Hash Map insert function.

--- hashtable.h
+++ hashtable.h
@@ -13,3 +13,15 @@ struct HTab
     size_t mask = 0;    // 2^n - 1
     size_t size = 0;
 };
+
+struct HMap
+{
+    HTab h1; // newer
+    HTab h2; // older
+    size_t resizing_pos = 0;
+};
+
+const size_t k_max_load_factor = 0;
+const size_t k_resizing_work = 128; // constant work
+
+void hm_insert(HMap *hmap, HNode *node);
