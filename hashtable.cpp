4e27a87292d4559603fa0bc25b4bda3ed4d16762
brettabamonte <abamonteb@gmail.com>
2024-03-04 21:58:29 -0500

    Add delete, search, destroy, size, and resizing functions for hash map.

--- hashtable.cpp
+++ hashtable.cpp
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <assert.h>
 
+const size_t k_max_load_factor = 0;
+const size_t k_resizing_work = 128; // constant work
+
 static void h_init(HTab *htab, size_t n)
 {
     assert(n > 0 && ((n - 1) & n) == 0);
@@ -53,4 +56,105 @@ static HNode *h_detach(HTab *htab, HNode **from)
     *from = node->next;
     htab->size--;
     return node;
+}
+
+void hm_insert(HMap *hmap, HNode *node)
+{
+    if (!hmap->h1.tab)
+    {
+        // Init a new hashtable of size 4 (2^2)
+        h_init(&hmap->h1, 4);
+    }
+
+    // Insert key into newer table
+    h_insert(&hmap->h1, node);
+
+    // Check the load factor of the newer table
+    if (!hmap->h2.tab)
+    {
+        size_t load_factor = hmap->h1.size / (hmap->h1.mask + 1);
+        if (load_factor >= k_max_load_factor)
+        {
+            // Create a larger table
+        }
+    }
+
+    // Move some keys into the newer table
+    hm_start_resizing(hmap);
+}
+
+static void hm_start_resizing(HMap *hmap)
+{
+    assert(hmap->h2.tab == NULL);
+
+    // Create a bigger hashtable and swap them
+    hmap->h2 = hmap->h1;
+    h_init(&hmap->h1, (hmap->h1.mask) * 2);
+    hmap->resizing_pos = 0;
+}
+
+static void hm_help_resizing(HMap *hmap)
+{
+    size_t nwork = 0;
+
+    while (nwork < k_resizing_work && hmap->h2.size > 0)
+    {
+        // Scan for nodes in ht2 and move them to ht1
+        HNode **from = &hmap->h2.tab[hmap->resizing_pos];
+
+        if (!*from)
+        {
+            hmap->resizing_pos++;
+            continue;
+        }
+
+        h_insert(&hmap->h1, h_detach(&hmap->h2, from));
+        nwork++;
+    }
+
+    if (hmap->h2.size == 0 && hmap->h2.tab)
+    {
+        // We are done. Free memory of older table.
+        free(hmap->h2.tab);
+
+        // Init a new struct for the older table
+        hmap->h2 = HTab();
+    }
+}
+
+HNode *hm_lookup(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *))
+{
+    hm_help_resizing(hmap);
+    HNode **from = h_lookup(&hmap->h1, key, eq);
+    from = from ? from : h_lookup(&hmap->h2, key, eq);
+    return from ? *from : NULL;
+}
+
+HNode *hm_pop(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *))
+{
+    hm_help_resizing(hmap);
+
+    if (HNode **from = h_lookup(&hmap->h1, key, eq))
+    {
+        return h_detach(&hmap->h1, from);
+    }
+
+    if (HNode **from = h_lookup(&hmap->h2, key, eq))
+    {
+        return h_detach(&hmap->h2, from);
+    }
+
+    return NULL;
+}
+
+size_t hm_size(HMap *hmap)
+{
+    return hmap->h1.size + hmap->h2.size;
+}
+
+void hm_destroy(HMap *hmap)
+{
+    free(hmap->h1.tab);
+    free(hmap->h2.tab);
+    *hmap = HMap();
 }
