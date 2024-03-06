7e6b588da660b2719650b9c8905f6577abedcdde
brettabamonte <abamonteb@gmail.com>
2024-03-06 13:38:02 -0500

    Add deserialization ability for TLV protocol

--- src/client.cpp
+++ src/client.cpp
@@ -15,6 +15,14 @@
 
 const size_t k_max_msg = 4096;
 
+enum {
+    SER_NIL = 0, //'NULL'
+    SER_ERR = 1, //Error code and message
+    SER_STR = 2, //string
+    SER_INT = 3, //int64
+    SER_ARR = 4, //array
+};
+
 static void msg(const char *msg)
 {
     fprintf(stderr, "%s\n", msg);
@@ -75,7 +83,7 @@ static int32_t send_req(int fd, const std::vector<std::string> &cmd)
     }
 
     char wBuf[4 + k_max_msg];
-    memcpy(wBuf, &len, 4);
+    memcpy(&wBuf[0], &len, 4);
     uint32_t n = cmd.size();
     memcpy(&wBuf[4], &n, len);
     size_t cur = 8;
@@ -89,6 +97,95 @@ static int32_t send_req(int fd, const std::vector<std::string> &cmd)
     }
 
     return write_all(fd, wBuf, 4 + len);
+}
+
+static int32_t on_response(const uint8_t *data, size_t size) {
+    if(size < 1) {
+        msg("bad response. Response size less than 1 byte.");
+        return -1;
+    }
+
+    switch(data[0]) {
+        case SER_NIL:
+            printf("(nil)\n");
+            return 1;
+        case SER_ERR:
+            if(size < 1 + 8) {
+                msg("bad response in SER_ERR.");
+                return -1;
+            }
+
+            {
+                int32_t code = 0;
+                uint32_t len = 0;
+                memcpy(&code, &data[1], 4);
+                memcpy(&len, &data[1 + 4], 4);
+
+                if(size < 1 + 8 + len) {
+                    msg("bad response");
+                    return -1;
+                }
+
+                printf("(err) %d %.*s\n", code, len, &data[1 + 8]);
+                return 1 + 8 + len;
+            }
+        case SER_STR:
+            if(size < 1 + 4) {
+                msg("bad response");
+                return -1;
+            }
+            {
+                uint32_t len = 0;
+                memcpy(&len, &data[1], 4);
+
+                if(size < 1 + 4 + len) {
+                    msg("bad response");
+                    return -1;
+                }
+
+                printf("(str) %.*s\n", len, &data[1 + 4]);
+                return 1 + 4 + len;
+            }
+        case SER_INT:
+            if(size < 1 + 8) {
+                msg("bad response");
+                return -1;
+            }
+            {
+                int64_t val = 0;
+                memcpy(&val, &data[1], 8);
+
+                printf("(int) %ld\n", val);
+                return 1 + 8;
+            }
+        case SER_ARR:
+            if(size < 1 + 4) {
+                msg("bad response");
+                return -1;
+            } 
+            {
+                uint32_t len = 0;
+                memcpy(&len, &data[1], 4);
+                printf("(arr) len=%u\n", len);
+                size_t array_bytes = 1 + 4;
+
+                for(uint32_t i = 0; i < len; ++i) {
+                    int32_t rv = on_response(&data[array_bytes], size - array_bytes);
+
+                    if(rv < 0) {
+                        return rv;
+                    }
+
+                    array_bytes += (size_t)rv;
+                }
+
+                printf("(arr) end\n");
+                return (int32_t)array_bytes;
+            }
+        default:
+            msg("bad response");
+            return -1;
+    }
 }
 
 static int32_t read_res(int fd)
@@ -125,17 +222,13 @@ static int32_t read_res(int fd)
     }
 
     // Read rescode and print result
-    uint32_t rescode = 0;
-    if (len < 4)
-    {
-        msg("bad response");
-        return -1;
+    int32_t rv = on_response((uint8_t *)&rBuf[4], len);
+    if(rv > 0 && (uint32_t)rv != len) {
+        msg("bad response.");
+        rv = -1;
     }
-    memcpy(&rescode, &rBuf[4], 4);
 
-    printf("server says: [%u] %.*s\n", rescode, len - 4, &rBuf[8]);
-
-    return 0;
+    return rv;
 }
 
 int main(int argc, char **argv)
