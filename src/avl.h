8dcc4e175aa0ccecd7540e0c5ff472ebf03fb64a
brettabamonte <abamonteb@gmail.com>
2024-03-06 22:42:25 -0500

    Add function signatures

--- src/avl.h
+++ src/avl.h
@@ -8,3 +8,12 @@ struct AVLNode {
     AVLNode *right = NULL;
     AVLNode *parent = NULL;
 };
+
+void avl_update(AVLNode *node);
+AVLNode *rotate_left(AVLNode *node);
+AVLNode *rotate_right(AVLNode *node);
+AVLNode *avl_fix_left(AVLNode *root);
+AVLNode *avl_fix_right(AVLNode *root);
+AVLNode *avl_fix(AVLNode *node);
+AVLNode *avl_del(AVLNode *node);
+void avl_init(AVLNode *node);
