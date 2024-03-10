4a9c7469fb3aadf93ef2aae8405059a486fc2bb1
brettabamonte <abamonteb@gmail.com>
2024-03-10 01:48:29 -0500

    Add implementation of str to int

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,3 +6,9 @@ static bool str2dbl(const std::string &s, double &out) {
     out = strtod(s.c_str(), &endp);
     return endp == s.c_str() + s.size() && !isnan(out);
 }
+
+static bool str2int(const std::string &s, int64_t &out) {
+    char *endp = NULL;
+    out = strtoll(s.c_str(), &endp, 10);
+    return endp == s.c_str() + s.size();
+}
