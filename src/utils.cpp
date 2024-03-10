5ac4ac9bd3d361050aa72c12f7912043d08624f9
brettabamonte <abamonteb@gmail.com>
2024-03-10 01:48:19 -0500

    Add implementation of str to dbl

---
+++ src/utils.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <stdlib.h>
+
+static bool str2dbl(const std::string &s, double &out) {
+    char *endp = NULL;
+    out = strtod(s.c_str(), &endp);
+    return endp == s.c_str() + s.size() && !isnan(out);
+}
